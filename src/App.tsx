f7563f5d540af0f31bf907ccdd0cb59369267f47
devYeonjun <alcls0026@gmail.com>
2022-05-20 16:23:13 +1200

    Add state & types

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 
+import { Todo } from "./todo.model";
 import TodoList from "./components/TodoList";
 import NewTodo from "./components/NewTodo";
 
 const App: React.FC = () => {
-	const todos = [{ id: "t1", text: "Finish the course" }];
+	const [todos, setTodos] = useState<Todo[]>([]);
 
-  const todoAddHandler = (text: string) => {
-    console.log(text);
-  }
+	const todoAddHandler = (text: string) => {
+		setTodos([{ id: Math.random().toString(), text: text }]);
+	};
 	return (
 		<div className="App">
 			<NewTodo onAddTodo={todoAddHandler} />
